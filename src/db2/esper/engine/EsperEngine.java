d31456b28c927c9fd054aa389d482ce0565147d1
dac <marco@dacol.me>
2014-03-27 16:04:59 +0100

    pronto per le query

--- src/db2/esper/engine/EsperEngine.java
+++ src/db2/esper/engine/EsperEngine.java
@@ -31,11 +31,29 @@ public class EsperEngine {
 				cepConfig);
 		EPRuntime cepRT = cep.getEPRuntime();
 		
-		String query = "SELECT * FROM PirwEvent ";
+		//GENERO GLI STREAM PER CATEGORIA DI SENSORE
 		Listener mylistener = new Listener();
-		EPStatement statement = cep.getEPAdministrator().createEPL(query);
-		statement.addListener(mylistener);
+		String query = null;
+		query = "INSERT INTO pirwEPL SELECT * FROM PirwEvent ";
+		EPStatement pirwEPL= cep.getEPAdministrator().createEPL(query);
+		//pirwEPL.addListener(mylistener);
 		
+		query = "INSERT INTO pircEPL SELECT * FROM PircEvent ";
+		EPStatement pircEPL = cep.getEPAdministrator().createEPL(query);
+		//pircEPL.addListener(mylistener);
+		
+		query = "INSERT into dwcEPL SELECT * FROM DwcEvent ";
+		EPStatement dwcEPL = cep.getEPAdministrator().createEPL(query);
+		//dwcEPL.addListener(mylistener);
+		
+		query = "INSERT into locationEPL SELECT * FROM LocationEvent ";
+		EPStatement locationEPL = cep.getEPAdministrator().createEPL(query);
+		
+		
+		//QUI CI VANNO LE QUERY PER TROVARE LE ANOMALIE
+		query = "SELECT * FROM locationEPL";
+		EPStatement onlyTrue = cep.getEPAdministrator().createEPL(query);
+		onlyTrue.addListener(mylistener);
 		
 		// The Configuration is meant only as an initialization-time object.
 
@@ -46,8 +64,9 @@ public class EsperEngine {
 //		cepStatement.addListener(new CEPListener());
 		
 		String file[] = new String[1];
-		file[0] = "data/A/stateDump.txt";
-		
+		//file[0] = "data/A/stateDump.txt";
+		file[0] = "data/A/LOC1395236845939.log";
+
 		EventGenerator evGen = new EventGenerator(cepRT,file);
 		
 		evGen.start();
